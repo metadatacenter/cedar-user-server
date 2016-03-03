ceae38c275243a3b605dea9d1eb7520424ee28af
egyedia <attila.egyedi@stanford.edu>
2016-03-02 16:48:34 -0800

    apiKey functionality working: create user profile, auto-generate apiKeys

---
+++ cedar-user-server-play/app/utils/DataServices.java
@@ -0,0 +1,31 @@
+package utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import controllers.UserServerController;
+import org.metadatacenter.server.service.UserService;
+import org.metadatacenter.server.service.mongodb.UserServiceMongoDB;
+import play.Configuration;
+import play.Play;
+
+import static org.metadatacenter.constant.ConfigConstants.MONGODB_DATABASE_NAME;
+import static org.metadatacenter.constant.ConfigConstants.USERS_COLLECTION_NAME;
+
+public class DataServices {
+
+  private static DataServices instance = new DataServices();
+  public static UserService<String, String, JsonNode> userService;
+
+  public static DataServices getInstance() {
+    return instance;
+  }
+
+  private DataServices() {
+    Configuration config = Play.application().configuration();
+    userService = new UserServiceMongoDB(
+        config.getString(MONGODB_DATABASE_NAME),
+        config.getString(USERS_COLLECTION_NAME));
+
+    UserServerController.injectUserService(userService);
+
+  }
+}
