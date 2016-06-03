9f01c7ca2c841fc18eda8b318f7ce386088731b4
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-06-03 15:49:27 -0700

    New Config replaces old one + global MAPPER

--- cedar-user-server-play/app/utils/DataServices.java
+++ cedar-user-server-play/app/utils/DataServices.java
@@ -1,33 +1,33 @@
 package utils;
 
 import controllers.UserServerController;
+import org.metadatacenter.config.CedarConfig;
+import org.metadatacenter.model.CedarNodeType;
 import org.metadatacenter.server.service.UserService;
 import org.metadatacenter.server.service.mongodb.UserServiceMongoDB;
-import play.Configuration;
-import play.Play;
-
-import static org.metadatacenter.constant.ConfigConstants.MONGODB_DATABASE_NAME;
-import static org.metadatacenter.constant.ConfigConstants.USERS_COLLECTION_NAME;
 
 public class DataServices {
 
   private static DataServices instance = new DataServices();
   private static UserService userService;
+  private static CedarConfig cedarConfig;
 
   public static DataServices getInstance() {
     return instance;
   }
 
   private DataServices() {
-    Configuration config = Play.application().configuration();
-    userService = new UserServiceMongoDB(
-        config.getString(MONGODB_DATABASE_NAME),
-        config.getString(USERS_COLLECTION_NAME));
-
+    cedarConfig = CedarConfig.getInstance();
+    userService = new UserServiceMongoDB(cedarConfig.getMongoConfig().getDatabaseName(),
+        cedarConfig.getMongoCollectionName(CedarNodeType.USER));
     UserServerController.injectUserService(userService);
   }
 
   public UserService getUserService() {
     return userService;
+  }
+
+  public static CedarConfig getCedarConfig() {
+    return cedarConfig;
   }
 }
